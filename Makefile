6d81c3553d871a974475f19e590074a7eb64268d
Ilter Kalach <ikalach@f1r6s14.codam.nl>
2025-10-31 13:56:00 +0100

    added header and makefile

---
+++ Makefile
@@ -0,0 +1,22 @@
+NAME = libftprintf.a
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+SRCS = ft_printf.c \
+
+OBJS = $(SRCS:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	ar rcs $(NAME) $(OBJS)
+
+%.o: %.cc
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJS)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
---
+++ printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikalach <ikalach@student.codam.nl>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 13:48:08 by ikalach           #+#    #+#             */
+/*   Updated: 2025/10/31 13:55:39 by ikalach          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+int	ft_printf(const char *, ...);
+
+#endif
