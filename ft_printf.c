e07f5e4c6fdb958b099f56818291358ee01e548b
Ilter Kalach <ikalach@f1r6s14.codam.nl>
2025-10-31 15:14:48 +0100

    printf works for integers and non arg cases

--- ft_printf.c
+++ ft_printf.c
@@ -6,10 +6,42 @@
 /*   By: ikalach <ikalach@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 14:47:32 by ikalach           #+#    #+#             */
-/*   Updated: 2025/10/30 14:48:06 by ikalach          ###   ########.fr       */
+/*   Updated: 2025/10/31 15:14:00 by ikalach          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printf(const char *, ...)
+#include "libft.h"
+#include "libftprintf.h"
+
+// CODE ALL TESTS FUNCTION THAT WILL CALL EVERY TEST FUNCTION FOR
+//% CASES AND CODE THE OTHER TEST CASE FUNCTIONS FOR NOW WE ONLY HAVE %D
+int	ft_printf(const char *fmt, ...)
 {
+	va_list	args;
+	char	*temp;
+
+	va_start(args, fmt);
+	while (*fmt != '\0')
+	{
+		if (*fmt == '%')
+		{
+			fmt++;
+			if (*fmt == 'd')
+			{
+				temp = ft_itoa(va_arg(args, int));
+				write(1, temp, ft_strlen(temp));
+				free(temp);
+			}
+			fmt++;
+		}
+		write(1, fmt, 1);
+		fmt++;
+	}
+	va_end(args);
+	return (1);
 }
+
+// int	main(void)
+// {
+// 	ft_printf("This is a %d test123", 12352163);
+// }
--- libftprintf.h
+++ libftprintf.h
@@ -1,21 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.h                                           :+:      :+:    :+:   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikalach <ikalach@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 13:48:08 by ikalach           #+#    #+#             */
-/*   Updated: 2025/10/31 13:55:39 by ikalach          ###   ########.fr       */
+/*   Updated: 2025/10/31 15:07:17 by ikalach          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
+# include <stdarg.h>
 # include <stddef.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <unistd.h>
 
-int	ft_printf(const char *, ...);
+int	ft_printf(const char *fmt, ...);
 
 #endif
